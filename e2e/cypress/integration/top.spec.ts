05bda9df69756b93da4da8688b14a0a3c8647567
megos <gmegos.27@gmail.com>
2021-01-03 16:26:03 +0900

    [Cypress] Fix test

--- e2e/cypress/integration/top.spec.ts
+++ e2e/cypress/integration/top.spec.ts
@@ -1,11 +1,17 @@
-context('Top page', () => {
-  it('has messages', () => {
+context('Flow', () => {
+  it('login - has data - logout', () => {
     cy.visit('/')
-
     cy.get('input[name=id]').type('user1')
     cy.get('input[name=password]').type('user1password')
     cy.contains('button', 'Login').click()
+    cy.contains('Top')
 
-    cy.contains('hi')
+
+    cy.contains('data: 5').click()
+    cy.contains('hello: 5')
+
+
+    cy.contains('button', 'Logout').click()
+    cy.contains('Login')
   })
 })
