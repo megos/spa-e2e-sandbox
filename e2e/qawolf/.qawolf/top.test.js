b76e1ad5087679f0868d72cf4fe63fed91d25453
megos <gmegos.27@gmail.com>
2021-01-10 19:46:05 +0900

    yarn qawolf create http://localhost:8080 top

---
+++ e2e/qawolf/.qawolf/top.test.js
@@ -0,0 +1,27 @@
+const qawolf = require("qawolf");
+
+let browser;
+let context;
+
+beforeAll(async () => {
+  browser = await qawolf.launch();
+  context = await browser.newContext();
+  await qawolf.register(context);
+});
+
+afterAll(async () => {
+  await qawolf.stopVideos();
+  await browser.close();
+});
+
+test("top", async () => {
+  const page = await context.newPage();
+  await page.goto("http://localhost:8080/", { waitUntil: "domcontentloaded" });
+  await page.click('[name="id"]');
+  await page.fill('[name="id"]', "user1");
+  await page.press('[name="id"]', "Tab");
+  await page.fill('[name="password"]', "user1password");
+  await page.click(".MuiButton-root");
+  await page.click("text=5");
+  await page.click(".MuiButton-root");
+});
