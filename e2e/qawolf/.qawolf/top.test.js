1597267c163a82997912f0cbc68c3ab004c113c3
megos <gmegos.27@gmail.com>
2021-01-10 23:24:51 +0900

    Apply Prettier

--- e2e/qawolf/.qawolf/top.test.js
+++ e2e/qawolf/.qawolf/top.test.js
@@ -1,30 +1,30 @@
-const { assertElementText } = require("qawolf");
-const qawolf = require("qawolf");
+const { assertElementText } = require('qawolf')
+const qawolf = require('qawolf')
 
-let browser;
-let context;
+let browser
+let context
 
 beforeAll(async () => {
-  browser = await qawolf.launch();
-  context = await browser.newContext();
-  await qawolf.register(context);
-});
+  browser = await qawolf.launch()
+  context = await browser.newContext()
+  await qawolf.register(context)
+})
 
 afterAll(async () => {
-  await qawolf.stopVideos();
-  await browser.close();
-});
+  await qawolf.stopVideos()
+  await browser.close()
+})
 
-test("top", async () => {
-  const page = await context.newPage();
-  await page.goto("http://localhost:8080/", { waitUntil: "domcontentloaded" });
-  await page.click('[name="id"]');
-  await page.fill('[name="id"]', "user1");
-  await page.press('[name="id"]', "Tab");
-  await page.fill('[name="password"]', "user1password");
-  await page.click(".MuiButton-root");
-  await page.click("text=5");
+test('top', async () => {
+  const page = await context.newPage()
+  await page.goto('http://localhost:8080/', { waitUntil: 'domcontentloaded' })
+  await page.click('[name="id"]')
+  await page.fill('[name="id"]', 'user1')
+  await page.press('[name="id"]', 'Tab')
+  await page.fill('[name="password"]', 'user1password')
+  await page.click('.MuiButton-root')
+  await page.click('text=5')
   await assertElementText(page, 'div', 'hello: 5')
-  await page.click(".MuiButton-root");
+  await page.click('.MuiButton-root')
   await assertElementText(page, 'h6', 'Login')
-});
+})
