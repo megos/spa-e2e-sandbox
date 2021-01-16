1211af71a2f7f081b28acc2a4913e03da3b4591b
megos <gmegos.27@gmail.com>
2021-01-16 18:43:09 +0900

    npx wdio config

---
+++ e2e/webdriverio/example.e2e.ts
@@ -0,0 +1,14 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        browser.url(`https://the-internet.herokuapp.com/login`);
+
+        $('#username').setValue(username);
+        $('#password').setValue(password);
+        $('button[type="submit"]').click(); 
+
+        expect($('#flash')).toBeExisting();
+        expect($('#flash')).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
